33be45328755017c3337d3b65e48031210766a4b
youssef salah <josalah118@gmail.com>
2025-09-24 14:07:40 +0300

    Add Cors

--- SurveyBasket.Api/DependencyInjection.cs
+++ SurveyBasket.Api/DependencyInjection.cs
@@ -16,6 +16,14 @@ public static class DependencyInjection
     {
         services.AddControllers();
 
+        services.AddCors(options =>
+            options.AddDefaultPolicy(builder =>
+                builder
+                .AllowAnyMethod()
+                .AllowAnyHeader()
+                .WithOrigins(configuration.GetSection("AllowedOrigins").Get<string[]>()!)
+        ));
+
         services.AddAuth(configuration);
 
         services.AddDatabase(configuration);
--- SurveyBasket.Api/Program.cs
+++ SurveyBasket.Api/Program.cs
@@ -15,6 +15,8 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseCors();
+
 app.UseAuthorization();
 
 app.MapControllers();
